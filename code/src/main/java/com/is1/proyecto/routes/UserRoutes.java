1cf67e68499f1cd2a96bf854e1e4b5ecffba64c2
Leon <leondellafiore2016@gmail.com>
2026-05-12 21:09:40 -0300

    Restyle login.mustache template, fix route path on UserRoutes.

--- code/src/main/java/com/is1/proyecto/routes/UserRoutes.java
+++ code/src/main/java/com/is1/proyecto/routes/UserRoutes.java
@@ -19,7 +19,7 @@ public class UserRoutes {
 
     public void register() {
         get("/",              controller::showLoginForm);
-        get("/user/create",   controller::showCreateForm);
+        get("/user/new",   controller::showCreateForm);
         get("/dashboard",     controller::showDashboard);
         get("/logout",        controller::logout);
         post("/user/new",     controller::create);
