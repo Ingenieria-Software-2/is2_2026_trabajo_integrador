1eaf633a050ed4225b4f42169346a2cf7ad44bab
Leon <leondellafiore2016@gmail.com>
2026-05-23 10:11:24 -0300

    Add Person dto

--- code/src/main/java/com/is1/proyecto/services/dto/PersonLoginDTO.java
+++ code/src/main/java/com/is1/proyecto/services/dto/PersonLoginDTO.java
@@ -1,6 +1,6 @@
 package com.is1.proyecto.services.dto;
 
-public class UserCreateDTO {
-    public String name;
+public class PersonLoginDTO {
+    public String username;
     public String password;
 }
